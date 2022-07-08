4084da2edce047f403d3accc44a9cf63ee52d22e
syifa sukma <syifasukmaramadhani@gmail.com>
2022-07-08 17:46:52 +0700

    update

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'htpps://parfait.lunarteam.net';
+    public $baseURL = 'https://parfait.lunarteam.net/';
 
     /**
      * --------------------------------------------------------------------------
